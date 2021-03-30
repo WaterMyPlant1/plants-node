2fb3b738bed0c2c94fb0dc7a58a06899ee46c8c7
hdm007 <harry.mardirossian13@gmail.com>
2021-03-30 00:15:05 -0400

    heroku push

--- api/data/db-config.js
+++ api/data/db-config.js
@@ -1,4 +1,9 @@
-const knex = require('knex')
-const configs = require('../../knexfile')
+const knex = require('knex');
 
-module.exports = knex(configs[process.env.NODE_ENV])
+const knexfile = require('../knexfile');
+
+
+const env = process.env.NODE_ENV || 'development';
+const configOptions = knexfile[env];
+
+module.exports = knex(configOptions);
--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -4,11 +4,23 @@ exports.up = async (knex) => {
       users.increments('user_id')
       users.string('user_username', 200).notNullable()
       users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
+      users.integer('phone_number', 320).notNullable()
+    })
+    .createTable('plants', (plants) => {
+      plants.increments('plant_id')
+      plants.string('nickname', 200).notNullable()
+      plants.string('species', 200).notNullable()
+      plants.integer('watering frequency').notNullable()
+      plants.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+
     })
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('plants')
   await knex.schema.dropTableIfExists('users')
 }
